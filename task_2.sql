d06b6ab57f2bb8b96ef92ef170c9e4e134102fcb
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-17 20:52:40 +0100

    added task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Create books table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- Create customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- Create orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
