9960869c04dfe3f17154891ce9804526ca1fd24d
fran utibe joseph <majesticfrank7@gmail.com>
2025-08-17 17:27:45 +0100

    added MYSQLServer.py.

---
+++ MYSQLServer.py
@@ -0,0 +1,25 @@
+import mysql.connector
+# Connection parameters
+host = "localhost"
+user = "root"
+password = "Wallet"
+
+# Attempt connection
+conn = mysql.connector.connect(
+    host=host,
+    user=user,
+    password=password
+)
+
+if conn.is_connected():
+    print("✅ Connection successful!")
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database checked/created successfully.")
+
+    # Close connection
+    cursor.close()
+    conn.close()
+    print("MySQL connection closed.")
+else:
+    print("❌ Connection failed!")
